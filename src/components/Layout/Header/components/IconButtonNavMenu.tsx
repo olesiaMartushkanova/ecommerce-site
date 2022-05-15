7ef48cc7ea7547d31b7449e14caad3af718d66b2
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2022-05-15 18:33:37 +1200

    iconButtonNavMenu

--- src/components/Layout/Header/components/IconButtonNavMenu.tsx
+++ src/components/Layout/Header/components/IconButtonNavMenu.tsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
 import Icon from '../../../Primitives/Icon';
 
 const IconButtonNavMenu = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const [iconSrc, setIconSrc] = useState('/images/menu-closed.svg');
+
+  const handleClick = () =>
+    isMenuOpened ? setIsMenuOpened(false) : setIsMenuOpened(true);
+
+  useEffect(() => {
+    const src = isMenuOpened
+      ? '/images/menu-opened.svg'
+      : '/images/menu-closed.svg';
+    setIconSrc(src);
+  }, [isMenuOpened]);
+
   return (
-    <Icon alt={'header navigation icon menu'} src='/images/menu-closed.svg' />
+    <Icon
+      alt={'header navigation icon menu'}
+      src={iconSrc}
+      isClickable={true}
+      onClick={handleClick}
+    />
   );
 };
 export default IconButtonNavMenu;
